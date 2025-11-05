76ff50987c540b11db6f85345adaa54032552c70
Evan <evan@ix.co.za>
2025-11-05 07:21:17 +0200

    Moved css and js into their own files

--- Hangman/Controllers/HomeController.cs
+++ Hangman/Controllers/HomeController.cs
@@ -4,29 +4,134 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hangman.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly List<string> _wordList = new List<string>
+		{
+				"acceptance",
+				"accident",
+				"administration",
+				"agony",
+				"aluminium",
+				"assessment",
+				"autonomy",
+				"ballet",
+				"basin",
+				"bathtub",
+				"beneficiary",
+				"bin",
+				"brag",
+				"branch",
+				"catalogue",
+				"charity",
+				"chauvinist",
+				"chimney",
+				"clothes",
+				"collect",
+				"concept",
+				"constant",
+				"correspondence",
+				"dare",
+				"deficiency",
+				"dentist",
+				"depression",
+				"deter",
+				"display",
+				"dividend",
+				"earthflax",
+				"elbow",
+				"even",
+				"export",
+				"fall",
+				"fast",
+				"flex",
+				"fold",
+				"goalkeeper",
+				"gold",
+				"harvest",
+				"help",
+				"homosexual",
+				"hostility",
+				"infinite",
+				"insist",
+				"jelly",
+				"justice",
+				"kidnap",
+				"kidney",
+				"king",
+				"lack",
+				"learn",
+				"lid",
+				"lifestyle",
+				"looting",
+				"maze",
+				"means",
+				"measure",
+				"menu",
+				"merit",
+				"mine",
+				"mourning",
+				"nature",
+				"need",
+				"neighbour",
+				"night",
+				"nose",
+				"officer",
+				"opposition",
+				"ordinary",
+				"organisation",
+				"organize",
+				"palace",
+				"palm",
+				"parachute",
+				"pat",
+				"penalty",
+				"philosophy",
+				"pray",
+				"rabbit",
+				"recommendation",
+				"record",
+				"reference",
+				"rifle",
+				"rock",
+				"sanctuary",
+				"service",
+				"settlement",
+				"shy",
+				"singer",
+				"soar",
+				"soprano",
+				"spread",
+				"suntan",
+				"surround",
+				"television",
+				"terrify",
+				"tourist",
+				"training"
+		};
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			List<string> list = _wordList.ToList();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
